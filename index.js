613a8a5f99c9b0926e7bd40b916827caaa9b8a58
shivamkumarsingh28 <110661272+shivamkumarsingh28@users.noreply.github.com>
2023-08-21 06:02:19 +0000

    JavaScript_AI_Health_Assist

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 const express = require('express');
-
-const imgext = require('tesseract.js');
-
-imgext.recognize('https://upload.wikimedia.org/wikipedia/commons/1/17/Text_entropy.png',
-  'eng',{ logger: m => console.log(m) }
-  ).then(({ data: { text } }) => {
-    console.log(text);
-  })
+const fs = require('fs');
+const multer = require('multer')
+const worker = require('tesseract.js');
 
 const app = express()
 const port = 3000
+app.use('/static', express.static('static'));
+
+app.set('view engine', 'ejs')
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  }, 
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({storage: storage}).single("avatar");
 
 app.get('/',(req, res) => {
-    res.send("Hello SaeeAM") 
+    res.render('index',{ title: 'Kratin Health AI', message: 'Hello SaeeAM!' })
 })
+
+app.post('/upload',(req, res) => {
+  upload(req, res, err => {
+    fs.readFile(`./uploads/${req.file.originalname}`, (err, data) =>{
+      if(err) return console.log('this is your error', err);
+      worker
+      .recognize(data, "eng", { logger:m => console.log(m)})
+      .then(({ data: { text } }) => {
+            // console.log(text);
+            res.send(text);
+      });
+    });
+  });
+});
 
 app.listen(port, ()=> {
     console.log(`Server start ${port}`)
